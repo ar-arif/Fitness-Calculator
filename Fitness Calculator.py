fa152aab18b5eade4ed0b1f8319070d2cdfc66dc
Md. Ariful Islam <56508132+ar-arif@users.noreply.github.com>
2020-07-10 20:18:12 +0600

    Add files via upload

---
+++ Fitness Calculator.py
@@ -0,0 +1,157 @@
+def ft_to_cm():
+	ft = float(input("\"ft\": "))
+	print("\"cm\":", ft*30.48)
+
+def kg_to_lbs():
+	ft = float(input("\"kg\": "))
+	print("\"lbs\":", ft*2.205)
+
+def lbs_to_kg():
+	lbs = float(input("\"lbs\": "))
+	print("\"kg\":", lbs/2.205)
+
+def pal():
+        print("Physical Activity Level (PAL):")
+        print("    (1) Desk Job / no Exercise")
+        print("    (2) Light Exercise 1-2 times/Week")
+        print("    (3) Moderate Exercise 2-3 times/Week")
+        print("    (4) Hard Exercise 3-5 times/Week")
+        print("    (5) Physical job or Hard Exercise 6-7 times/Week")
+        print("    (6) Professional Athelete")
+        pal = int(input("Enter your (PAL) number: "))
+        if pal==1:
+                return 1.2
+        elif pal==2:
+                return 1.4
+        elif pal==3:
+                return 1.6
+        elif pal==4:
+                return 1.75
+        elif pal==5:
+                return 2
+        elif pal==6:
+                return 2.4
+        else:
+                print("Invalid Number!!")
+        
+def bmr():
+	gender = input("Gender M/F: ")
+	age = int(input("Age: "))
+	height = float(input("Height \"cm\": "))
+	weight = float(input("Weight \"kg\": "))
+
+
+	BMR_Male = 10 * weight + 6.25 * height - 5 * age + 5
+	BMR_Female = 10 * weight + 6.25 * height - 5 * age - 161
+
+	if gender=="m" or gender=="M":
+		return BMR_Male
+	elif gender=="f" or gender=="F":
+		return BMR_Female
+
+	
+def maintenance_calories():
+        gender = input("Gender M/F: ")
+        age = int(input("Age: "))
+        height = float(input("Height \"cm\": "))
+        weight = float(input("Weight \"kg\": "))
+
+        BMR_Male = 10 * weight + 6.25 * height - 5 * age + 5
+        BMR_Female = 10 * weight + 6.25 * height - 5 * age - 161
+
+        if gender=="m" or gender=="M":
+                return BMR_Male * pal()
+        elif gender=="f" or gender=="F":
+                return BMR_Female * pal()
+def macro():
+        gender = input("Gender M/F: ")
+        age = int(input("Age: "))
+        height = float(input("Height \"cm\": "))
+        weight = float(input("Weight \"kg\": "))
+        print("What's your Goal:")
+        print("    (1) Muscle Gain")
+        print("    (2) Maintenance")
+        print("    (3) Fat Loss")
+        goal = int(input("Enter your Goal number: "))
+
+        BMR_Male = 10 * weight + 6.25 * height - 5 * age + 5
+        BMR_Female = 10 * weight + 6.25 * height - 5 * age - 161
+
+
+        fat_gm = 0.8 * weight
+        fat_cal = fat_gm * 9
+        protein_gm = 2 * weight
+        protein_cal = protein_gm * 4
+
+        if gender=="m" or gender=="M":
+                calories =  BMR_Male * pal()
+                if goal==1:
+                        calories += 500
+                elif goal==2:
+                        calories = calories
+                elif goal==3:
+                        calories -= 500
+                print("\nCalories:",int(calories))
+                fat_per = (fat_cal * 100) / calories
+                print("Fat     :", str(int(fat_cal))+"cal", "|", str(int(fat_gm))+"gm", "|", str(int(fat_per))+"%")
+                protein_per = (protein_cal * 100) / calories
+                print("Proteins:", str(int(protein_cal))+"cal", "|", str(int(protein_gm))+"gm", "|", str(int(protein_per))+"%")
+                carb_cal = calories - (fat_cal+protein_cal)
+                carb_gm = calories/4
+                carb_per = (carb_cal * 100) / calories
+                print("Carbs   :", str(int(carb_cal))+"cal", "|", str(int(carb_gm))+"gm", "|", str(int(carb_per))+"%")
+        elif gender=="f" or gender=="F":
+                calories =  BMR_Female * pal()
+                if goal==1:
+                        calories += 500
+                elif goal==2:
+                        calories = calories
+                elif goal==3:
+                        calories -= 500
+                print("\nCalories:",int(calories))
+                fat_per = (fat_cal * 100) / calories
+                print("Fat     :", str(int(fat_cal))+"cal", "|", str(int(fat_gm))+"gm", "|", str(int(fat_per))+"%")
+                protein_per = (protein_cal * 100) / calories
+                print("Proteins:", str(int(protein_cal))+"cal", "|", str(int(protein_gm))+"gm", "|", str(int(protein_per))+"%")
+                carb_cal = calories - (fat_cal+protein_cal)
+                carb_gm = calories/4
+                carb_per = (carb_cal * 100) / calories
+                print("Carbs   :", str(int(carb_cal))+"cal", "|", str(int(carb_gm))+"gm", "|", str(int(carb_per))+"%")
+
+
+
+while True:
+        print()
+        print("(1) ft to cm")
+        print("(2) kg to lbs")
+        print("(3) lbs to kg")
+        print("(4) Physical Activity Level (PAL)")
+        print("(5) Basal Metabolic Rate (BMR)")
+        print("(6) Maintenance Calories")
+        print("(7) Macro")
+        print("(0) exit")
+        a = int(input())
+        if a==1:
+                print("\nft to cm")
+                ft_to_cm()
+        elif a==2:
+                print("\nkg to lbs")
+                kg_to_lbs()
+        elif a==3:
+                print("\nlbs to kg")
+                lbs_to_kg()
+        elif a==4:
+                print("\nPAL")
+                print("\nPAL:", pal())
+        elif a==5:
+                print("\nBasal Metabolic Rate (BMR)")
+                print("\nBMR:", bmr(), "cal")
+        elif a==6:
+                print("\nMaintenance Calories")
+                print("\nMaintenance Calories:", maintenance_calories(), "cal")
+        elif a==7:
+                macro()
+        elif a==0:
+                break
+        else:
+                print("Invalid number!!")
